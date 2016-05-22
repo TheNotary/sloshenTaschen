1df80b15de2fe188725bd4feb0c0bc572d63d94a
nohman20 <nohman20@gmail.com>
2016-05-21 22:15:42 -0700

    Working middleware

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
-var multer = require('multer'),
-  bodyParser = require('body-parser'),
-  path = require('path')
+var multer = require('multer')
+var bodyParser = require('body-parser')
+var path = require('path')
 
 var app = new express()
 app.use(bodyParser.json())
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
 // ///
-app.use(express.static())
+// app.use(express.static())
 app.use('/public', express.static(path.join(__dirname, '/public')))
 
 app.get('/', function (req, res) {
@@ -24,26 +24,6 @@ app.get('/hello', function (req, res) {
 
 // res.sendFile('views/canvas.js')
 })
-
-/*app.post('/', multer({ dest: './uploads/'}).single('upl'), function (req, res) {
-  console.log(req.body) // form fields
-  /* example output:
-  { title: 'abc' }
-   */
-// console.log(req.file) // form files
-/* example output:
-          { fieldname: 'upl',
-            originalname: 'grumpy.png',
-            encoding: '7bit',
-            mimetype: 'image/png',
-            destination: './uploads/',
-            filename: '436ec561793aa4dc475a88e84776b1b9',
-            path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
-            size: 277056 }
- 
-res.status(204).end()
-}) 
-*/
 
 var port = 3000
 app.listen(port, function () { console.log('listening on port ' + port); })
